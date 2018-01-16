63994a3d860a1938bf0e9ce386207946148ecb05
Ubuntu <yangalexandery@gmail.com>
2018-01-16 17:24:11 +0000

    renamed hihat files, added clap sound, remapped zxc keys

--- src/models/NoteInfoList.ts
+++ src/models/NoteInfoList.ts
@@ -14,10 +14,11 @@ export class NoteInfoList {
         makeINoteInfo(11, "snare-drum-2", "Snare", getSoundFileUrl("snare-drum-2")),
         makeINoteInfo(12, "snare-drum-3", "Snare", getSoundFileUrl("snare-drum-3")),
         makeINoteInfo(13, "snare-drum-4", "Snare", getSoundFileUrl("snare-drum-4")),
-        makeINoteInfo(20, "hihat-1", "Hihat", getSoundFileUrl("hihat-1")),
-        makeINoteInfo(21, "hihat-2", "Hihat", getSoundFileUrl("hihat-2")),
-        makeINoteInfo(22, "hihat-3", "Hihat", getSoundFileUrl("hihat-3")),
-        makeINoteInfo(23, "hihat-4", "Hihat", getSoundFileUrl("hihat-4")),
+        makeINoteInfo(20, "hihat-drum-1", "Hihat", getSoundFileUrl("hihat-drum-1")),
+        makeINoteInfo(21, "hihat-drum-2", "Hihat", getSoundFileUrl("hihat-drum-2")),
+        makeINoteInfo(22, "hihat-drum-3", "Hihat", getSoundFileUrl("hihat-drum-3")),
+        makeINoteInfo(23, "hihat-drum-4", "Hihat", getSoundFileUrl("hihat-drum-4")),
+        makeINoteInfo(30, "clap-drum-1", "Clap", getSoundFileUrl("clap-drum-1")),
     ];
 }
 
@@ -29,8 +30,8 @@ function getSoundFileUrl(noteName: string): string {
 export function getStarterNotes(): INoteInfo[] {
     return [
         NoteInfoList.notes[1],
-        NoteInfoList.notes[3],
-        NoteInfoList.notes[7],
+        NoteInfoList.notes[11],
+        NoteInfoList.notes[8],
     ]
 }
 
