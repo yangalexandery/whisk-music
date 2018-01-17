04cd8fb5ec3bd28dcdec05e3511d46773016b107
Ubuntu <yangalexandery@gmail.com>
2018-01-17 18:51:24 +0000

    changed instruments to more frequently used ones

--- src/components/pages/PlayerPageComponent.tsx
+++ src/components/pages/PlayerPageComponent.tsx
@@ -55,15 +55,16 @@ export class PlayerPageComponent extends React.Component<IPlayerPageComponentPro
 
     public static readonly pianoInstrOptions = [
         {label: "acoustic_grand_piano", name: "Piano"},
-        {label: "clavinet", name: "Clavinet"},
-        {label: "marimba", name: "Marimba"},
+        {label: "acoustic_bass", name: "Bass"},
+        {label: "acoustic_guitar_nylon", name: "Guitar"},
         {label: "alto_sax", name: "Alto Sax"},
         {label: "clarinet", name: "Clarinet"},
         {label: "flute", name: "Flute"},
         {label: "cello", name: "Cello"},
         {label: "violin", name: "Violin"},
         {label: "steel_drums", name: "Steel Drums"},
-        {label: "woodblock", name: "Woodblock"},
+        {label: "synth_brass_1", name: "Synth"},
+        {label: "synth_bass_1", name: "Synth Bass"},
     ]
 
     constructor(props: IPlayerPageComponentProps) {
