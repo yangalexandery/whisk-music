d4c29f8014850b456540dd328cd555e22b12735b
Ubuntu <yangalexandery@gmail.com>
2018-01-19 20:34:09 +0000

    small structural changes to keyboard range

--- src/components/pages/PlayerPageComponent.tsx
+++ src/components/pages/PlayerPageComponent.tsx
@@ -50,6 +50,7 @@ export class PlayerPageComponent extends React.Component<IPlayerPageComponentPro
     keyToNotes: {[key: string]: any[]};
 
     soundOptionComponent: InstrumentOption;
+    octave: number;
     octaveUp: boolean;
 
 
@@ -106,6 +107,7 @@ export class PlayerPageComponent extends React.Component<IPlayerPageComponentPro
         this.noteKeyboardManager = new NoteKeyboardManager(this);
         this.noteKeyboardManager.attachListeners();
         this.octaveUp = false;
+        this.octave = 3;
 
         // Keyboard listener to play sounds
         // TODO: REFACTOR THIS PART
@@ -285,10 +287,10 @@ export class PlayerPageComponent extends React.Component<IPlayerPageComponentPro
 
     private getNoteToPlay(k: string) {
         if (this.octaveUp) {
-            console.log(NoteMap[k].substring(0, NoteMap[k].length - 1) + (parseInt(NoteMap[k].substring(NoteMap[k].length - 1)) + 1).toString());
-            return NoteMap[k].substring(0, NoteMap[k].length - 1) + (parseInt(NoteMap[k].substring(NoteMap[k].length - 1)) + 1).toString();
+            // console.log(NoteMap[k].note + (NoteMap[k].octave + 1).toString());
+            return NoteMap[k].note + (NoteMap[k].octave + this.octave + 1).toString();
         }
-        return NoteMap[k];
+        return NoteMap[k].note + (NoteMap[k].octave + this.octave).toString();
     }
 
     private downKey(k: string) {
